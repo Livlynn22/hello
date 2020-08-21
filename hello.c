7a5f1cda62f8bcbfffff90f46c3a14eb5f26bcbc
Olivia <olivia.froelicher@huskers.unl.edu>
2020-08-21 01:06:43 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/** 
+* Author: Olivia Froelicher 
+* Date: 2020/08/18
+* 
+* A simple hello world program in C 
+* 
+*/ 
+#include <stdlib.h> 
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+ 
+printf(" Olivia Froelicher\n"); 
+
+
+ return 0; 
+  }
